3c7ed892f495c0d51f6c5203273d675c5a023532
VITOR SANTANA DE JESUS <vitor.alunos@cfp127aluno.local>
2023-07-29 11:16:43 -0300

    Hello vr-1

---
+++ Hello.java
@@ -0,0 +1,10 @@
+class hello {
+	public static void main(String[] args){
+		System.out.print("Hello word");
+		System.out.print("A aula esta Boa !!");
+		System.out.print ln("teste");
+		/comentar/
+		
+		
+	}
+}
